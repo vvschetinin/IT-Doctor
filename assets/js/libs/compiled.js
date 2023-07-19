ae5615850e198be164e9be48c8152d236ca9b695
Vladlen Schetinin <born-1965@mail.ru>
2023-07-19 23:03:44 +0300

    first commit

---
+++ assets/js/libs/compiled.js
@@ -0,0 +1,1 @@
+class People{sayhi(){console.log("Hi")}}let b=new People;b.sayhi();
---
+++ assets/js/main.js
@@ -0,0 +1,2 @@
+class Person{constructor(t,s,i){this.name=t,this.age=s,this.salary=i}toString(){return`Имя: ${this.name}\nВозраст: ${this.age}\nЗарплата: ${this.salary}`}}let foo=17;if(foo){setTimeout((function(){}),500)}
+//# sourceMappingURL=main.js.map
