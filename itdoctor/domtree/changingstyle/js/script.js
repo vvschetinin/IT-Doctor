49c890796d986a3431d7da0f94658c1231940e5c
Vladlen Schetinin <vvschetinin@gmail.com>
2023-08-02 17:12:35 +0300

    DOM

--- itdoctor/domtree/changingstyle/js/script.js
+++ itdoctor/domtree/changingstyle/js/script.js
@@ -1,1 +1,8 @@
 'use strict';
+
+let element = document.getElementById('element');
+element.style.backgroundColor = '#f00'; // Установка стилей элемента
+console.log(element);
+
+let elements = document.querySelectorAll('.workarea ul > li:first-child');
+console.log(elements);
