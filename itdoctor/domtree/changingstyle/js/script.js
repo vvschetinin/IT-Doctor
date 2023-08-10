5ae2e8cf03fc9f89423439186fd7c02af1c59d78
Vladlen Schetinin <vvschetinin@gmail.com>
2023-08-10 20:43:00 +0300

    Fix

--- itdoctor/domtree/changingstyle/js/script.js
+++ itdoctor/domtree/changingstyle/js/script.js
@@ -1,8 +1,1 @@
 'use strict';
-
-let element = document.getElementById('element');
-element.style.backgroundColor = '#f00'; // Установка стилей элемента
-console.log(element);
-
-let elements = document.querySelectorAll('.workarea ul > li:first-child');
-console.log(elements);
